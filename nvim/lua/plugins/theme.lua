a3d3fbb8181964e48aa884b7739277ff8e137743
Ollie Moss <olliemoss321@gmail.com>
2025-07-17 20:58:39 +1200

    Added hyprland, bash and nvim config

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,24 @@
+return {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
+	{
+		"rebelot/kanagawa.nvim",
+	},
+	{
+		"sho-87/kanagawa-paper.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+}
